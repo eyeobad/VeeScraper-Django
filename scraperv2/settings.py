ae876a6b9c79fb358ae673b9ad58ab46a7a8864d
eyeobad <ogudavictor492@gmail.com>
2025-07-11 12:54:38 +0100

    adjusted setting for production

--- scraperv2/settings.py
+++ scraperv2/settings.py
@@ -23,21 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ifo6r!ml%0d=*__0^b5n-2%dt-p&=%bqap5w*a==-+b3h5l6c2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-
+DEBUG = False
 ALLOWED_HOSTS = ['Veescraper.onrender.com']
 
 
 STATIC_ROOT  = BASE_DIR / 'staticfiles'
-
-
 
 
 # Application definition
